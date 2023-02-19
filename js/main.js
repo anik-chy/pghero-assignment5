6a826c3245cfeddcb1108f255d9532f2799d5a21
anik-chy <anikchy2140@gmail.com>
2023-02-19 08:11:33 +0600

    basic result added

--- js/main.js
+++ js/main.js
@@ -16,6 +16,20 @@ function resetCardColor(cardName) {
   card.style.backgroundColor = "#fff";
 }
 
+// reset fields
+function resetFields(field1, field2) {
+  field1.value = "";
+  field2.value = "";
+}
+
+// result card text formater
+function resultTestFormater(objName, calculatedArea){
+    const names = objName.split('-')
+    let name = names[0].charAt(0).toUpperCase()+names[0].slice(1);
+    const finalStr = name.padEnd(15,' ') + (Math.round(calculatedArea * 100) / 100).toFixed(2)+' cm²';
+
+    return finalStr;
+}
 // area calculation
 function calArea(multiFactor, objName) {
   const field1 = document.getElementById(objName + "-f1");
@@ -24,14 +38,23 @@ function calArea(multiFactor, objName) {
   const field1Val = Number(field1.value);
   const field2Val = Number(field2.value);
 
-  console.log(field1Val + " " + field2Val);
-
-  if (Object.is(field1Val,NaN) || Object.is(field2Val,NaN)) {
+  if (Object.is(field1Val, NaN) || Object.is(field2Val, NaN)) {
     window.alert("Please Recheck Your Inputs....");
-    field1.value = "";
-    field2.value = "";
+    resetFields(field1, field2);
   } else {
-    field1.value = "";
-    field2.value = "";
+    let calculatedArea = multiFactor * field1Val * field2Val;
+    const areaList = document.getElementById("area-list");
+
+    const str = resultTestFormater(objName, calculatedArea);
+    console.log(str);
+
+    const li = document.createElement("li");
+    li.setAttribute("id", objName + "-li");
+    li.innerText = str;
+    li.innerHTML = `${str} <button type="button" class="btn btn-primary px-1";
+    onclick="calArea(1,'parallel-card')">Convert to m²</button>`;
+    areaList.appendChild(li);
+
+    resetFields(field1, field2);
   }
 }
