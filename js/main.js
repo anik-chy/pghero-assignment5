70c18223970a90d3f55c72246170fd0a98608145
anik-chy <anikchy2140@gmail.com>
2023-02-19 06:42:13 +0600

    field validation added

--- js/main.js
+++ js/main.js
@@ -12,6 +12,26 @@ function changeRandColor(cardName) {
 
 // reset the hover effect of the cards
 function resetCardColor(cardName) {
-    const card = document.getElementById(cardName);
-    card.style.backgroundColor = "#fff" ;
+  const card = document.getElementById(cardName);
+  card.style.backgroundColor = "#fff";
+}
+
+// area calculation
+function calArea(multiFactor, objName) {
+  const field1 = document.getElementById(objName + "-f1");
+  const field2 = document.getElementById(objName + "-f2");
+
+  const field1Val = Number(field1.value);
+  const field2Val = Number(field2.value);
+
+  console.log(field1Val + " " + field2Val);
+
+  if (Object.is(field1Val,NaN) || Object.is(field2Val,NaN)) {
+    window.alert("Please Recheck Your Inputs....");
+    field1.value = "";
+    field2.value = "";
+  } else {
+    field1.value = "";
+    field2.value = "";
   }
+}
